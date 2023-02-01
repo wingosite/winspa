b9e3117c3456682e511f88981ee7c029fe8dcdfe
Bronson Zi Ying QIU <bronsonqiu@aslcn.com.cn>
2023-02-01 15:25:43 +0800

    feat: 删减一些无用的文件和代码

--- README.md
+++ README.md
@@ -23,4 +23,3 @@
 ## License
 
 MIT
-
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="wrapper">Hi</div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
